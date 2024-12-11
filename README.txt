d80e2652dd584794f1722184414d00af1844f24d
Abhishek Saini <99207474+Abhishek6394@users.noreply.github.com>
2024-12-11 19:50:19 +0530

    custom plugin
    added the file custom plugin

---
+++ README.txt
@@ -0,0 +1,64 @@
+=== Login Template Plugin ===
+Contributors: Abhishek Saini
+Tags: login, templates, custom login, login page, WordPress login, customization
+Requires at least: 5.0
+Tested up to: 6.0
+Stable tag: trunk
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Donate link: https://www.wellgotrip.com/
+
+== Description ==
+
+The **Login Template Plugin** allows WordPress site administrators to select from a variety of pre-built login page templates, providing an easy way to customize the login experience without coding. 
+
+With this plugin, you can:
+- Choose from a variety of customizable login page templates.
+- Change the look and feel of your login screen with a few clicks.
+- Manage templates directly from the WordPress dashboard.
+- Easily switch between templates without affecting your site's functionality.
+
+Whether you want a simple login screen or a more complex, styled design, this plugin makes it easy to tailor the user experience to match your brand.
+
+== Features ==
+
+- Select and apply pre-designed login page templates.
+- No coding required to customize the look of your login screen.
+- Choose templates from the plugin settings page in the WordPress dashboard.
+- Easily switch between templates.
+- Lightweight and easy to use.
+
+== Installation ==
+
+1. Upload the `login-template-plugin` folder to your WordPress `wp-content/plugins/` directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Navigate to the plugin settings under **Settings > Login Template** to choose your preferred login template.
+4. Save your settings to apply the new template.
+
+== Frequently Asked Questions ==
+
+= How do I change the login page template? =
+
+Once you activate the plugin, go to **Settings > Login Template** and choose your preferred template. After saving your settings, the new login page template will be applied.
+
+= Can I add custom templates? =
+
+Currently, the plugin supports a selection of pre-built templates, but you can extend it by adding custom templates to the plugin directory.
+
+= Is this plugin compatible with other login customization plugins? =
+
+It is compatible with most login customization plugins. However, certain features might conflict with other login page customizations, so we recommend testing it in a staging environment before using it on your live site.
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial release of the Login Template Plugin.
+* Users can select and apply login page templates from the WordPress admin.
+
+== Screenshots ==
+
+1. Screenshot of the plugin settings page where users can choose a login template.
+https://prnt.sc/sRtNzEZLbb1V
+2. Screenshot of the login page using the selected template.
+https://prnt.sc/3Hqorfelgmjh
+https://prnt.sc/HN6Z_P3miex4
---
+++ custom-login-template.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://https://www.wellgotrip.com/
+ * @since             1.0.0
+ * @package           Custom_Login_Template
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Custom Login Template
+ * Plugin URI:        https://https://www.wellgotrip.com/
+ * Description:       This plugin allows users to select from various pre-existing login page templates directly within the WordPress dashboard. Choose a template from a list of options, and easily customize the look and feel of your site's login page without the need for custom coding.
+ * Version:           1.0.0
+ * Author:            Abhishek Saini
+ * Author URI:        https://https://www.wellgotrip.com//
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       custom-login-template
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Currently plugin version.
+ * Start at version 1.0.0 and use SemVer - https://semver.org
+ * Rename this for your plugin and update it as you release new versions.
+ */
+define( 'CUSTOM_LOGIN_TEMPLATE_VERSION', '1.0.0' );
+
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-custom-login-template-activator.php
+ */
+function activate_custom_login_template() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-custom-login-template-activator.php';
+	Custom_Login_Template_Activator::activate();
+}
+
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-custom-login-template-deactivator.php
+ */
+function deactivate_custom_login_template() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-custom-login-template-deactivator.php';
+	Custom_Login_Template_Deactivator::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_custom_login_template' );
+register_deactivation_hook( __FILE__, 'deactivate_custom_login_template' );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-custom-login-template.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_custom_login_template() {
+
+	$plugin = new Custom_Login_Template();
+	$plugin->run();
+
+}
+run_custom_login_template();
