041f759ffc5b41799fcfca2a8051483aa8af5a06
Abhishek Saini <99207474+Abhishek6394@users.noreply.github.com>
2025-01-03 21:00:32 +0530

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -19,7 +19,7 @@
  * For more information, see the following discussion:
  * https://github.com/tommcfarlin/WordPress-Plugin-Boilerplate/pull/123#issuecomment-28541913
  *
- * @link       https://https://www.wellgotrip.com/
+ * @link       https://www.wellgotrip.com/
  * @since      1.0.0
  *
  * @package    Custom_Login_Template
